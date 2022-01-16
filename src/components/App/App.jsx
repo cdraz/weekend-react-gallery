118783b1f489b8b08c59b5d53dfdac12257b62eb
Christopher Razidlo <cd.razidlo@gmail.com>
2022-01-16 11:17:20 -0600

    base-mode complete

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import 'react-bootstrap/dist/react-bootstrap.min'
 import GalleryList from '../GalleryList/GalleryList';
 
 function App() {
